5ff235f504f30eae88aeea57915ce4a2841e2b99
lingronghai <lingronghai@foxmail.com>
2022-07-30 14:47:10 +0800

    样式调整：重置样式、全局样式、字体图标

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { HashRouter } from 'react-router-dom'
 
+import ResetCss from '@/assets/styles/reset-css'
+import IconStyle from '@/assets/iconfont/iconfont'
+
 import store from './store'
 import Router from './router'
 
-const App = () => {
-  console.log('App')
-  return (
-    <React.StrictMode>
-      <Provider store={store}>
-        <HashRouter>
-          <Router />
-        </HashRouter>
-      </Provider>
-    </React.StrictMode>
-  )
-}
+const App = () => (
+  <React.StrictMode>
+    <Provider store={store}>
+      <HashRouter>
+        <ResetCss />
+        <IconStyle />
+        <Router />
+      </HashRouter>
+    </Provider>
+  </React.StrictMode>
+)
 
 export default App
