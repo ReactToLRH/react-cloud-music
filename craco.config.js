48328e4fb47a5b6bd3e4d9ecbbfcb82e19e4b035
lingronghai <lingronghai@foxmail.com>
2022-08-05 09:46:03 +0800

    调整 postcss-px-to-viewport 相关配置

--- craco.config.js
+++ craco.config.js
@@ -15,20 +15,22 @@ module.exports = {
         postcssOptions: {
           plugins: [
             require('postcss-px-to-viewport-8-plugin')({
-              unitToConvert: 'px', // 需要转换的单位，默认为"px"
-              viewportWidth: 750, // 设计稿的视口宽度
-              unitPrecision: 5, // 单位转换后保留的精度
-              propList: ['*', '!font-size'], // 能转化为vw的属性列表,!font-size表示font-size后面的单位不会被转换
-              viewportUnit: 'vw', // 希望使用的视口单位
-              fontViewportUnit: 'vw', // 字体使用的视口单位
-              // 需要忽略的CSS选择器，不会转为视口单位，使用原有的px等单位。
-              selectorBlackList: ['keep-px]'], // 下面配置表示类名中含有'keep-px'都不会被转换
-              minPixelValue: 1, // 设置最小的转换数值，如果为1的话，只有大于1的值会被转换
-              mediaQuery: false, // 媒体查询里的单位是否需要转换单位
-              replace: true, //  是否直接更换属性值，而不添加备用属性
-              exclude: [/node_modules/], // 忽略某些文件夹下的文件或特定文件，例如 'node_modules' 下的文件
-              include: [/src/], // 如果设置了include，那将只有匹配到的文件才会被转换
-              landscape: false, // 是否添加根据 landscapeWidth 生成的媒体查询条件 @media (orientation: landscape)
+              unitToConvert: 'px', // 要转化的单位
+              viewportWidth: 375, // UI设计稿的宽度
+              viewportHeight: 667, // UI
+              unitPrecision: 6, // 转换后的精度，即小数点位数
+              // propList: 当有些属性的单位我们不希望转换的时候，可以添加在数组后面，并在前面加上!号，如propList: ["*","!letter-spacing"],这表示：所有css属性的属性的单位都进行转化，除了letter-spacing的
+              propList: ['*'], // 指定转换的css属性的单位，* 代表全部css属性的单位都进行转换, !font-size 表示font-size后面的单位不会被转换
+              viewportUnit: 'vw', // 指定需要转换成的视窗单位，默认vw
+              fontViewportUnit: 'vw', // 指定字体需要转换成的视窗单位，默认vw
+              // 转换的黑名单，在黑名单里面的我们可以写入字符串，只要类名包含有这个字符串，就不会被匹配。比如selectorBlackList: ['wrap'],它表示形如wrap,my-wrap,wrapper这样的类名的单位，都不会被转换
+              selectorBlackList: ['ignore'], // 指定不转换为视窗单位的类名，
+              minPixelValue: 1, // 默认值1，小于或等于1px则不进行转换
+              mediaQuery: true, // 是否在媒体查询的css代码中也进行转换，默认false
+              replace: true, // 是否转换后直接更换属性值
+              exclude: [/node_modules/], // 设置忽略文件，用正则做目录名匹配
+              // include: [/node_modules/], // 如果设置了 include，那将只有匹配到的文件才会被转换
+              landscape: false, // 是否处理横屏情况
               landscapeUnit: 'vw', // 横屏时使用的单位
               landscapeWidth: 1338 // 横屏时使用的视口宽度
             })
