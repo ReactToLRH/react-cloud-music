5ccaefd570887885321b7abbea1e13ebf5a64650
lingronghai <lingronghai@foxmail.com>
2022-08-11 21:07:12 +0800

    推荐页面优化

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,6 +1,6 @@
 import React, { memo, useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
-import { useDispatch, useSelector, shallowEqual } from 'react-redux'
+import { useSelector, useDispatch, shallowEqual } from 'react-redux'
 import { forceCheck } from 'react-lazyload'
 
 import Scroll from '@/baseUI/Scroll'
@@ -12,10 +12,11 @@ import { Content } from './style'
 import { getBannerRequestAsync, getRecommendListRequestAsync } from './store'
 
 const Recommend = () => {
+  const dispatch = useDispatch()
+
   const bannerList = useSelector(state => state.recommend.banner, shallowEqual)
   const recommendList = useSelector(state => state.recommend.recommendList, shallowEqual)
-
-  const dispatch = useDispatch()
+  const playList = useSelector(state => state.player.playList, shallowEqual)
 
   useEffect(() => {
     dispatch(getBannerRequestAsync())
@@ -23,7 +24,7 @@ const Recommend = () => {
   }, [dispatch])
 
   return (
-    <Content>
+    <Content play={playList.length}>
       <Scroll className="list" onScroll={forceCheck}>
         <div>
           <Slider bannerList={bannerList} />
